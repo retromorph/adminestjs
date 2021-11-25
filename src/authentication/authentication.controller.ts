4f8c89f864f75b06e01972f01bf6ee0b0e427745
Nikita Musalov <38955182+retromorph@users.noreply.github.com>
2021-11-25 10:08:57 +0300

    Crud (#2)
    * crud service, controller and module
    
    * user service, shared + minor fixes in genericcrud module and tsconfig
    
    * all'
    
    * authenticaation

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+
+import { LoginUserDto } from "../user/dtos/loginUser.dto";
+import { AuthenticationService } from "./authentication.service";
+import { TokenInterface } from "./interfaces/token.interface";
 
 @Controller("authentication")
-export class AuthenticationController {}
+export class AuthenticationController {
+  public constructor(
+    private readonly authenticationService: AuthenticationService,
+  ) {}
+
+  @Post("login")
+  public async login(@Body() loginUser: LoginUserDto): Promise<TokenInterface> {
+    return this.authenticationService.login(loginUser);
+  }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
-import { UserController } from "./user.controller";
 import { UserEntity } from "./user.entity";
 import { UserService } from "./user.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [UserService],
-  controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
